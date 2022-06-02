f1c1916b9e18e31785866b366d2e876d5b11b8c2
jeonghyeonkwon <givejeong2468@gmail.com>
2022-06-02 15:45:51 +0900

    중복 아이디 아이디 비밀번호 빈 값 검증

--- chat-server/app.ts
+++ chat-server/app.ts
@@ -1,46 +1,50 @@
-import * as express from 'express';
-import * as morgan from 'morgan';
-import * as session  from 'express-session'
-import {webSocket} from './socket';
-import * as dotenv from 'dotenv'
-const {sequelize} = require('./models');
-import userRouter from './routes/user';
+import * as express from "express";
+import * as morgan from "morgan";
+import * as session from "express-session";
+import { webSocket } from "./socket";
+import * as dotenv from "dotenv";
+import * as cors from "cors";
+const { sequelize } = require("./models");
+import userRouter from "./routes/user";
 dotenv.config();
-
 
 const app = express();
 
 sequelize
-    .sync({force: true})
-    .then(() => {
-        console.log('연결 성공');
-    })
-    .catch(() => {
-        console.log('에러');
-    });
+  .sync({ force: true })
+  .then(() => {
+    console.log("연결 성공");
+  })
+  .catch(() => {
+    console.log("에러");
+  });
 
+app.set("port", process.env.PORT || 8000);
 
-app.set('port',process.env.PORT || 3000);
-
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+app.use("/api/user", userRouter);
 
-app.use('/user',userRouter);
-
-
-app.use((err:any,req:express.Request,res:express.Response,next:express.NextFunction)=>{
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     console.error(err);
-    return res.status(400).send({msg:err.message});
-})
+    return res.status(400).send({ msg: err.message });
+  }
+);
 
-const server = app.listen(app.get('port'),()=>{
-    console.log(app.get('port'),'번 포트에서 대기중');
-})
+const server = app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
 
-server
+server;
 // webSocket(server);
--- chat-server/controllers/user.ts
+++ chat-server/controllers/user.ts
@@ -2,27 +2,39 @@ import { NextFunction, Response, Request } from "express";
 import { v4 } from "uuid";
 
 import { sequelize } from "../models";
-import User from '../models/user';
-const bcrypt = require('bcrypt');
-export const createUser = async (req:Request,res:Response,next:NextFunction)=>{
-    const transaction = await sequelize.transaction();
-    try{
-        const {userId,userPassword} = req.body;
-        console.log(req.body);
-        const encodedPassword = await bcrypt.hash(userPassword,14);
-        const createUser = await User.create({
-            userId:userId,
-            userPassword:encodedPassword,
-            userRandomId:v4(),
-        },{transaction});
-
-        transaction.commit();
-        res.status(201).send({
-            msg: createUser.userRandomId
-        })
-    }catch(err){
-        transaction.rollback();
-        next(err);
+import User from "../models/user";
+const bcrypt = require("bcrypt");
+export const createUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const transaction = await sequelize.transaction();
+  try {
+    const { userId, userPassword } = req.body;
+    if (!userId || !userPassword) {
+      throw Error("아이디와 비밀번호 중 빈 값이 있습니다.");
     }
-    
-}
+    const isExistUser = await User.findOne({ where: { userId } });
+    if (isExistUser) {
+      throw Error(`사용 중인 아이디 입니다`);
+    }
+    const encodedPassword = await bcrypt.hash(userPassword, 14);
+    const createUser = await User.create(
+      {
+        userId: userId,
+        userPassword: encodedPassword,
+        userRandomId: v4(),
+      },
+      { transaction }
+    );
+
+    transaction.commit();
+    res.status(201).send({
+      msg: createUser.userRandomId,
+    });
+  } catch (err) {
+    transaction.rollback();
+    next(err);
+  }
+};
